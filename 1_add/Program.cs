9954e40b6bbddaa71eccd2d48ce53398eb5f0f09
yuriy645 <mryurii@yahoo.com>
2022-04-30 15:29:52 +0200

    Add project files.

---
+++ 1_add/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1_add
+{
+    class Program
+    {//Создайте массив чисел размерностью в 1 000 000 или более. Используя генератор случайных
+     //  чисел, проинициализируйте этот массив значениями.Создайте PLINQ запрос, который
+     //   позволит получить все нечетные числа из исходного массива.
+        static void Main()
+        {
+            int[] array = new int[10000000];
+
+            Random random = new Random(); 
+
+            Parallel.For( 0, 10000000, (i) => array[i] = random.Next(-99, +99) );
+
+
+            //ParallelQuery<int> negatives = from element in array.AsParallel()
+            //                               where element < 0
+            //                               select element;
+
+            //PLINQ запрос, который
+     //     позволит получить все нечетные числа из исходного массива
+
+            ParallelQuery<int> negatives = from element in array.AsParallel()
+                                           where element % 2 != 0
+                                           select element;
+
+            foreach (int element in negatives)
+                Console.Write(element + "   ");
+
+         
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2_Invoke/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _2_Invoke
+{//Создайте два метода, которые будут выполняться в рамках параллельных задач. Организуйте
+  //  вызов этих методов при помощи Invoke таким образом, чтобы основной поток программы
+ //   (метод Main) не остановил свое выполнение.
+
+    //Invoke- это из класса Parallel
+    class Program
+    {
+        static void MyTask1()
+        {
+            Console.WriteLine("MyTask1: запущен.");
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(10);
+                Console.Write("+");
+            }
+            Console.WriteLine("MyTask1: завершен.");
+        }
+
+        static void MyTask2()
+        {
+            Console.WriteLine("MyTask2: запущен.");
+            for (int i = 0; i < 80; i++)
+            {
+                Thread.Sleep(10);
+                Console.Write("-");
+            }
+            Console.WriteLine("MyTask2: завершен.");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Основной поток запущен.");
+            
+
+            // Action action = new Action(() => Parallel.Invoke(MyTask1, MyTask2));
+            // Task.Factory.StartNew(action);
+
+            Task.Factory.StartNew(() => Parallel.Invoke(MyTask1, MyTask2)); //  то же самое
+
+            Console.WriteLine("Основной поток завершен.");
+
+            Console.ReadKey();
+        }
+    }
+}
